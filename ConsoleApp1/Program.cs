f15d7a6af8a1496efc23b9d9f66cb7851057f50a
sober607 <sober607@gmail.com>
2020-03-09 19:19:48 +0200

    First commit lesson9 task 4

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    extern alias Lesson1;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lesson1.Lesson9_11_Task4_2_.Class1 externalClass = new Lesson1.Lesson9_11_Task4_2_.Class1();
+            externalClass.MyMethod();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson9(11)Task4(2)/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson9_11_Task4_2_
+{
+    public class Class1
+    {
+        public void MyMethod()
+        {
+            Console.WriteLine("This is MyMethod of Lesson9_11_Task4_2_ namespace");
+        }
+    }
+}
